d1aafc4e15a15ebe18f28277a2f7e64251cec1bb
Simio Astuto <norbertrodrigueza90@gmail.com>
2025-10-29 13:51:56 -0300

    Fix: token dinámico en axios para autenticación

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-const token = localStorage.getItem("token");
-
 const api = axios.create({
-  baseURL: "https://workflow-manager-backend-z51a.onrender.com/api",
-  headers: {
-    Authorization: token ? `Bearer ${token}` : undefined
+  baseURL: "https://workflow-manager-backend-z51a.onrender.com/api"
+});
+
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
+  return config;
 });
 
 export default api;
